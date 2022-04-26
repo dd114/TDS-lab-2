f5d94087335abde8aca6b66417adb437bac2eb2d
dd114 <40179423+dd114@users.noreply.github.com>
2022-04-27 02:23:56 +0500

    minor edits

--- lab2.cpp
+++ lab2.cpp
@@ -1,11 +1,11 @@
-﻿#include <iostream>
-#include "ExplicitDifferenceSchemeMethod.h"
+﻿#include "ExplicitDifferenceSchemeMethod.h"
 
 using namespace std;
 int main() {
     ExplicitDifferenceSchemeMethod ExplicitDifferenceScheme;
-    cout << ExplicitDifferenceScheme.ValuesOfU(10, 0.8) << endl;
-    cout << ExplicitDifferenceScheme.exactSolution(10, 0.8) << endl;
-    cout << "Hello World!" << endl;
+    cout << ExplicitDifferenceScheme.ValuesOfU(1, 0.8, 200, 10) << endl;
+    cout << ExplicitDifferenceScheme.ValuesOfU(1, 0.8, 1e+1, 10) << endl;
+    cout << ExplicitDifferenceScheme.exactSolution(1, 0.8) << endl;
+    //cout << "Hello World!" << endl;
 }
 
