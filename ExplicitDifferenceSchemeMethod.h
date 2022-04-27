fd72636163fef85e991dc1a530363d2ca94cbea1
dd114 <40179423+dd114@users.noreply.github.com>
2022-04-27 14:11:16 +0500

    output switch was added

--- ExplicitDifferenceSchemeMethod.h
+++ ExplicitDifferenceSchemeMethod.h
@@ -27,13 +27,29 @@ public:
         this->numberOfPointByX = numberOfPointByX;
         this->numberOfPointByT = numberOfPointByT;
 
-        this->grid = vector<vector<double>>(numberOfPointByT, vector<double>(numberOfPointByX));
+        //this->grid = vector<vector<double>>(numberOfPointByT, vector<double>(numberOfPointByX));
 
         Calculate(t, x);
         int firstPoint = floor(x / stepByX);
         //int secondPoint = firstPoint + 1;
         vector<double> lastTimeLayer = grid.back();
         return lastTimeLayer[firstPoint];
+    }
+
+    double maximumOfInaccuracyGrid(double t, double x, int numberOfPointByT, int numberOfPointByX) {
+        ValuesOfU(t, x, numberOfPointByT, numberOfPointByX);
+
+        double maxInaccuracy = 0;
+        for (int i = 0; i < grid.size(); i++) {
+            for (int j = 0; j < grid[i].size(); j++) {
+                double exactValueOfU = exactSolution(i * stepByT, j * stepByX);
+                double inaccuracy = grid[i][j] - exactValueOfU;
+                if (inaccuracy > maxInaccuracy) {
+                    maxInaccuracy = inaccuracy;
+                }
+            }
+        }
+        return maxInaccuracy;
     }
 
     ~ExplicitDifferenceSchemeMethod() {
@@ -45,16 +61,17 @@ private:
         return stepByT <= stepByX * stepByX / (2 * k0);
     }
 
-    virtual void Calculate(double t, double x) override {
-
-        this->stepByX = 1. / (numberOfPointByX - 1);
-        this->stepByT = t / (numberOfPointByT - 1);
+    void additionalStabilityVerification(double t, bool output) {
+        if (stepByX < 0 || stepByT < 0) {
+            cerr << "STEP IS NEGATIVE" << endl;
+            throw - 1;
+        }
 
         if (!stepByTÑheck(stepByT, stepByX)) {
             this->numberOfPointByT = ceil(2 * t * k0 / (stepByX * stepByX) + 1);
             this->stepByT = t / (numberOfPointByT - 1);
-            this->grid = vector<vector<double>>(numberOfPointByT, vector<double>(numberOfPointByX, 0));
-            cout << "VALUE numberOfPointByT WILL BE CHANGED ON " << numberOfPointByT << endl;
+            if (output) 
+                cout << "VALUE numberOfPointByT WILL BE CHANGED ON " << numberOfPointByT << endl;
             //if (numberOfPointByT < 0) cerr << "numberOfPointByT is negative" << endl;
         }
 
@@ -62,6 +79,16 @@ private:
             cerr << "ADDITIONAL VERIFICATION WASN'T PASSED" << endl;
             throw stepByT;
         }
+
+        this->grid = vector<vector<double>>(numberOfPointByT, vector<double>(numberOfPointByX, 0));
+    }
+
+    virtual void Calculate(double t, double x) override {
+
+        this->stepByX = 1. / (numberOfPointByX - 1);
+        this->stepByT = t / (numberOfPointByT - 1);
+
+        additionalStabilityVerification(t, false);
 
 
         for (int i = 0; i < grid[0].size(); i++)
--- lab2.cpp
+++ lab2.cpp
@@ -2,10 +2,18 @@
 
 using namespace std;
 int main() {
+
+    //1.1
     ExplicitDifferenceSchemeMethod ExplicitDifferenceScheme;
+    //1.2
     double x = 0.8;
     cout << ExplicitDifferenceScheme.ValuesOfU(1, x, 200, 10) << endl;
-    cout << ExplicitDifferenceScheme.ValuesOfU(1, x, 1e+1, 10) << endl;
+    //1.3
+    x = 0.8;
+    //cout << ExplicitDifferenceScheme.ValuesOfU(1, x, 2, 10) << endl;
+    cout << ExplicitDifferenceScheme.maximumOfInaccuracyGrid(1, x, 2, 10) << endl;
+    //cout << ExplicitDifferenceScheme.ValuesOfU(1, x, 2, 150) << endl;
+    cout << ExplicitDifferenceScheme.maximumOfInaccuracyGrid(1, x, 2, 150) << endl;
     cout << "Exact solution = " << ExplicitDifferenceScheme.exactSolution(1, x) << endl;
 
     //cout << ExplicitDifferenceScheme.ValuesOfU(1, x, 1e+1, 5) << endl;
